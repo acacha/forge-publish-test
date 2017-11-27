bf3f50af72060410f9d3c38fe8fdb90f4fdbc5be
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-11-27 18:11:41 +0100

    Sync

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,7 +1,5 @@
-cd /home/forge/forgepublish.sergitur.2dam.iesebre.com
-git pull origin master
+#!/usr/bin/env bash
 cd /home/forge/forgepublish.sergitur.2dam.iesebre.com/forge-publish
 git pull origin master
 cd /home/forge/forgepublish.sergitur.2dam.iesebre.com
 composer dumpautoload
-echo "" | sudo -S service php7.1-fpm reload
---
+++ post-reload.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo "Post RELOAD test!"
